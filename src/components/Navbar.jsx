cadfca026032f323a42b6cbe79d654d1455157bf
katesgo <75998710+katesgo@users.noreply.github.com>
2024-01-23 22:57:47 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
 					<img
 						src={toggle ? close : menu}
 						alt="menu"
-						className="w-[28px] h-[28px] objet-contain cursor-pointer"
+						className="w-[28px] h-[28px] object-contain cursor-pointer"
 						onClick={() => setToggle(!toggle)}
 					/>
 					<div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
